fb8183e958f93543174b9cbcc4fdaf9102bf8c81
PC <anniekh94@gmail.com>
2024-04-25 11:06:08 +0300

    initial commit

---
+++ com/hillel/sokolova/basic120424/src/main/java/lessons/lesson01/Application.java
@@ -0,0 +1,8 @@
+package lessons.lesson01;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Anna");
+        System.out.println("Sokolova");
+    }
+}
