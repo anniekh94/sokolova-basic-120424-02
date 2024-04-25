19a720ce2f1ac9549aef508ce103896780dafa4f
PC <anniekh94@gmail.com>
2024-04-25 11:24:54 +0300

    rename out

--- com/hillel/sokolova/basic120424/src/main/java/lessons/lesson03/Main.java
+++ com/hillel/sokolova/basic120424/src/main/java/lessons/lesson03/Main.java
@@ -2,9 +2,8 @@ package lessons.lesson03;
 
 public class Main {
     public static void main(String[] args) {
-        //This is shop-online app
 
-        System.out.println("This is shop-online app.");
+        System.out.println("Shop-online for customers.");
     }
 
 }
