18eb285dfcb483862127fdce7971dc0054516884
PC <anniekh94@gmail.com>
2024-05-03 18:49:57 +0300

    add homework 06

--- com/hillel/sokolova/basic120424/src/main/java/lessons/lesson06/Main.java
+++ com/hillel/sokolova/basic120424/src/main/java/lessons/lesson06/Main.java
@@ -2,7 +2,12 @@ package lessons.lesson06;
 
 public class Main {
     public static void main(String[] args) {
-
-
+        int sum = 0;
+        for (int i = 1; i <= 6; i++) {
+            sum += i;
+            System.out.printf("%s) Num is %s, sum is %s %n", i, i, sum);
+        }
+        System.out.println("------------------------");
+        System.out.println("Sum of numbers is " + sum);
     }
 }
